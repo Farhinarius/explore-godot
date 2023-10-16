96640c64024872b139d36b4cb0f6972735e75bad
Farhinarius <farhat3027@gmail.com>
2023-10-16 20:32:19 +0300

    prepare cube controller for doing movement in 3d

--- Code/Control/CubeController.cs
+++ Code/Control/CubeController.cs
@@ -19,47 +19,4 @@ public partial class CubeController : CharacterBody3D
     private Vector3 _moveDirection = Vector3.Zero;
     private Vector3 _velocity = Vector3.Zero;
     private Vector3 _rotation = Vector3.Zero;
-
-    public override void _PhysicsProcess(double delta)
-    {
-        _moveDirection.X = Input.GetAxis(InputMapping.LeftCrossHorizontalNegative, InputMapping.LeftCrossHorizontalPositive);
-
-        _moveDirection.Z = Input.GetAxis(InputMapping.LeftCrossVerticalPositive,
-            InputMapping.LeftCrossVerticalNegative);
-
-        _moveDirection = _moveDirection.Rotated(Vector3.Up, _springArm.Rotation.Y).Normalized();                // related to camera
-
-        _velocity.X = _moveDirection.X * _speed;
-        _velocity.Z = _moveDirection.Z * _speed;
-        _velocity.Y -= _gravityForce * (float)delta;
-
-        var justLanded = IsOnFloor() && _snapVector == Vector3.Zero;
-        var jumping = IsOnFloor() && Input.IsActionJustPressed(InputMapping.Jump);
-
-        if (jumping)
-        {
-            _velocity.Y = _jumpStrength;
-            _snapVector = Vector3.Zero;
-        }
-        else if (justLanded)
-        {
-            _snapVector = Vector3.Down;
-        }
-
-        //if (_velocity.Length() > 0.2)
-        //{
-        //    var lookDirection = new Vector2(_velocity.Z, _velocity.X);
-        //    _rotation.X = lookDirection.Angle();
-        //}
-
-
-        Velocity = _velocity;
-        MoveAndSlide();
-        ApplyFloorSnap();
-    }
-
-    public override void _Process(double delta)
-    {
-        _springArm.Position = Position;
-    }
 }
